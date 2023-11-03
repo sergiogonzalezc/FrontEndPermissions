1796d727b8a01066c3fe87428144cf35d80ea265
Sergio Gonzalez <sergio.gonzalez.c@gmail.com>
2023-11-02 22:47:44 -0300

    fix port

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isDev = command === "serve";
+export default defineConfig({
+  plugins: [react()],
 
-  return {
-    plugins: [react()],
-    // we defined wish port the proyect will run on when execute: "npm run preview", and expose port 8080
-    server: {
-      host: true,
-      strictPort: true,
-      port: isDev ? 8085 : 8081,
-    },
-    preview: {
-      host: true,
-      strictPort: true,
-      port: isDev ? 8085 : 8081,
-    },
-  };
-});
+  // we defined wish port the proyect will run on when execute: "npm run preview", and expose port 8080
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8081
+  },
+  preview: {
+    host: true,
+    strictPort: true,
+    port: 8081
+  }
+})
